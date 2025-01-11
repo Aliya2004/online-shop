1129892fcde7844b0aadb0510493169926c0f860
Aliya <alia_0104@icloud.com>
2025-01-11 18:34:08 +0500

    Make удалить interactive

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-export const cart = [];
+export let cart = [
+  {
+    id: 'first',
+    quantity: 1,
+  },
+  {
+    id: 'second',
+    quantity: 1,
+  },
+];
+
+export function removeFromCart(id) {
+  let newCart = [];
+  cart.forEach((cartItem) => {
+    if (id !== cartItem.id) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+}
---
+++ scripts/shop-cart.js
@@ -0,0 +1,48 @@
+import { cart, removeFromCart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
+let cartSummaryHTML = '';
+
+cart.forEach((cartItem) => {
+  const productId = cartItem.id;
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  cartSummaryHTML += `
+    <div class="order__item js-order-item-${matchingProduct.id}" >
+            <div class="order__container-img">
+              <img
+                class="order__item-img"
+                src="${matchingProduct.image}"
+                alt=""
+              />
+            </div>
+
+            <div class="order__item-text">
+              <p>${matchingProduct.name}</p>
+              <p>Цена: ${matchingProduct.price} ₽</p>
+              <p>Количество: ${matchingProduct.quantity}</p>
+              <div class="order__item-btns">
+                <button class="delete-btn" data-product-id="${matchingProduct.id}" >Удалить</button>
+                <button>Добавить</button>
+              </div>
+            </div>
+          </div>
+    `;
+});
+
+document.querySelector('.order__items').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.delete-btn').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const productId = btn.dataset.productId;
+    removeFromCart(productId);
+    const container = document.querySelector(`.js-order-item-${productId}`);
+    container.remove();
+  });
+});
